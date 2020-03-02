7ff4fa518d5b7fe15acae5b36c02a02d6f01f263
Mykola Mokhnach <mokhnach@gmail.com>
2020-03-02 21:08:26 +0100

    Add JDK12

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -x
 
 tmpfile=$(mktemp /tmp/out.XXXXXX)
 java -jar ./target/verify*.jar "./tests/assets/tiny.apk" > "$tmpfile" 2>&1 || true
-if ! grep -q "Exception" "$tmpfile"; then
+if ! grep -q 'java.lang.Exception' "$tmpfile"; then
   exit 1
 fi
 java -jar ./target/verify*.jar "./tests/assets/tiny.s.apk" || exit 1
